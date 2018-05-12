fee8ff1928b74230c0e60b199352ef558533f476
landy8530 <landy8530@hotmail.com>
2018-05-12 08:01:21 +0800

    Initial commit

---
+++ src/main/java/com/landy/singleton/Singleton.java
@@ -0,0 +1,22 @@
+package com.landy.singleton;
+
+/**
+ * Created by Landy on 2018/5/12.
+ *
+ * 单例模式实现方式有好多种，但大部分都会有多线程环境下的问题；
+ * 使用内部类可以避免这个问题，因为在多线程环境下，jvm对一个类的初始化会做限制，
+ * 同一时间只会允许一个线程去初始化一个类，这样就从虚拟机层面避免了大部分单例实现的问题，可以尝试下
+ *
+ */
+public class Singleton {
+    private static class SingletonHolder {
+        public static final Singleton INSTANCE = new Singleton();
+    }
+
+    private Singleton (){}
+
+    public static final Singleton getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+
+}
---
+++ src/main/java/com/landy/singleton/Test.java
@@ -0,0 +1,16 @@
+package com.landy.singleton;
+
+/**
+ * Created by Landy on 2018/5/12.
+ * 测试单例类
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        Singleton singleton1 = Singleton.getInstance();
+        Singleton singleton2 = Singleton.getInstance();
+        if (singleton1 == singleton2)
+            System.out.println("same");
+    }
+
+}
