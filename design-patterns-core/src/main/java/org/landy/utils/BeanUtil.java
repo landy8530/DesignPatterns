62f05c5e0ffff8bd2d99e50d303a88c59e9d1c7b
chowsanity <344177653@qq.com>
2021-07-29 11:24:11 +0800

    解决并发公用校验器链，导致校验器链丢失校验器的问题

--- design-patterns-core/src/main/java/org/landy/utils/BeanUtil.java
+++ design-patterns-core/src/main/java/org/landy/utils/BeanUtil.java
@@ -2,7 +2,12 @@ package org.landy.utils;
 
 import org.apache.commons.beanutils.BeanUtils;
 import org.apache.commons.beanutils.PropertyUtils;
+import org.springframework.util.FastByteArrayOutputStream;
 
+import java.io.ByteArrayInputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 
 public class BeanUtil {
@@ -43,6 +48,30 @@ public class BeanUtil {
         }
 
         return null;
+    }
+
+    /**
+     * 序列化后拷贝流的方式克隆<br>
+     * 对象必须实现Serializable接口
+     *
+     * @param <T> 对象类型
+     * @param obj 被克隆对象
+     * @return 克隆后的对象
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T cloneByStream(T obj) {
+        if (!(obj instanceof Serializable)) {
+            return null;
+        }
+        final FastByteArrayOutputStream byteOut = new FastByteArrayOutputStream();
+        try (ObjectOutputStream out = new ObjectOutputStream(byteOut)) {
+            out.writeObject(obj);
+            out.flush();
+            final ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(byteOut.toByteArray()));
+            return (T) in.readObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
